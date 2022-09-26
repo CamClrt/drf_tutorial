eed3caf6dc4a2d5cd037e534396dfd08e4254308
CamClrt <camille.clarret@gmail.com>
2022-09-26 10:35:24 +0200

    add basic permissions

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -5,9 +5,12 @@ from snippets.models import Snippet
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    # We could have also used CharField(read_only=True) here
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
 
 
 class UserSerializer(serializers.ModelSerializer):
